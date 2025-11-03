d65742fb5ed8080bf362868d5a28869cc9a000e3
Kalum Ost <kalumost@gmail.com>
2025-11-02 23:34:13 -0500

    Initial GUI Layout; may be replaced with a fully viewed list of checkboxes later...

--- CART/CARTLib/examples/GenericClassification/GenericClassificationTask.py
+++ CART/CARTLib/examples/GenericClassification/GenericClassificationTask.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from typing import Optional
 
+import ctk
 import qt
+
 from CARTLib.core.TaskBaseClass import TaskBaseClass, DataUnitFactory, D
 from CARTLib.examples.GenericClassification.GenericClassificationUnit import GenericClassificationUnit
 from CARTLib.utils.config import ProfileConfig
@@ -13,13 +15,81 @@ class GenericClassificationGUI:
         # The task (logic) this GUI should be bound too
         self.bound_task = bound_task
 
+    def setup(self) -> qt.QFormLayout:
+        """
+        Build the GUI's contents, returning its layout for later use
+        """
+        # Initialize the layout
+        formLayout = qt.QFormLayout()
+
+        # Build the combobox
+        self._setupCheckbox(formLayout)
+
+        # Return the layout
+        return formLayout
+
+    def _setupCheckbox(self, layout: qt.QFormLayout):
+        # Generate a label for this combobox
+        label = qt.QLabel("Classifications:")
+        layout.addWidget(label)
+
+        # Make a sub-layout to align everything within
+        subLayout = qt.QHBoxLayout()
+
+        # Generate the CTK checkbox combobox
+        comboBox = ctk.ctkCheckableComboBox()
+
+        # Ensure that changes to the combobox reflect in the data unit
+        comboBox.checkedIndexesChanged.connect(
+            lambda c=comboBox: print(c.currentText.split("\n"))
+        )
+
+        # Track it for later
+        self.comboBox = comboBox
+
+        # Insert it into the layout
+        subLayout.addWidget(comboBox)
+
+        # Add an "addition" button
+        addButton = qt.QToolButton()
+        addButton.setText("+")
+
+        # When the button is pressed, generate the new class prompt
+        addButton.clicked.connect(
+            # TODO
+            lambda: print("=" * 100)
+        )
+
+        # Add it to the sub-layout
+        subLayout.addWidget(addButton)
+
+        # Place the sub layout into the main one
+        dummyWidget = qt.QWidget()
+        dummyWidget.setLayout(subLayout)
+        layout.addWidget(dummyWidget)
+
     @property
     def current_unit(self) -> GenericClassificationUnit:
         return self.bound_task.current_unit
 
     def syncWithTask(self):
-        # TODO
-        pass
+        # Disable signals to reduce spam
+        self.comboBox.blockSignals(True)
+
+        # Reset the combobox with the current contents of the task + data unit
+        self.comboBox.clear()
+
+        # Add all elements from the bound task, checking those already present in the data unit
+        new_options = self.bound_task.class_options
+        for i, o in enumerate(new_options):
+            self.comboBox.addItem(i)
+            if i in self.current_unit.classes:
+                self.comboBox.setCheckState(i, True)
+
+        # Restore signals to allow implicit synchronization
+        self.comboBox.blockSignals(False)
+
+        # TODO; Depending on config, adding classes in data unit not already registered
 
 @cart_task("Generic Classification")
 class GenericClassificationTask(TaskBaseClass[GenericClassificationUnit]):
@@ -45,8 +115,17 @@ class GenericClassificationTask(TaskBaseClass[GenericClassificationUnit]):
         # Currently managed data unit
         self.current_unit: Optional[GenericClassificationUnit] = None
 
+        # Currently available classes
+        self.class_options: set[str] = set()
+
     def setup(self, container: qt.QWidget):
-        pass
+        # Create and track the GUI
+        self.gui = GenericClassificationGUI(self)
+        gui_layout = self.gui.setup()
+        container.setLayout(gui_layout)
+
+        # Update the GUI to match our state
+        self.gui.syncWithTask()
 
     def receive(self, data_unit: GenericClassificationUnit):
         # Track the data unit for later
